fbc100056667578b3d8c4b095cfdbf3fb2f7c0db
DCode967 <olivosimancadilan@gmail.com>
2026-08-27 22:59:37 -0500

    Estructura, diseño, interactividad y resposive de mi portafolio

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+//Menu Hamburguesa del nav
+
 const menuToggle = document.querySelector("#menu-toggle");
 const nav = document.querySelector("#nav");
 
 menuToggle.addEventListener("click", () => {
     nav.classList.toggle("active");
 
+  
+
 });
+
+
+//Barras de nivel para habilidades
+const skillsSection = document.querySelector(".skills");
+const progressBars = document.querySelectorAll(".skill-progress");
+
+const observer = new IntersectionObserver((entries) => {
+
+    if (entries[0].isIntersecting) {
+
+        progressBars.forEach((bar) => {
+
+            const progress = bar.dataset.progress;
+
+            bar.style.width = progress + "%";
+
+        });
+
+        observer.unobserve(skillsSection);
+    }
+
+}, {
+    threshold: 0.3
+});
+
+observer.observe(skillsSection);
+
